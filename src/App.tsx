be2772ca706fa1076a42ee518b32ea8078102bec
daniel-dubon-dev <danieldubonxd@gmail.com>
2023-11-14 15:54:32 -0600

    changing css styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,7 @@ function App() {
 
 	return (
 		<>
-			<h1>Vite + React</h1>
-			<div className="card">
+			<div>
 				<button
 					onClick={() => {
 						setCount(count => count + 1)
@@ -22,9 +21,6 @@ function App() {
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	)
 }
